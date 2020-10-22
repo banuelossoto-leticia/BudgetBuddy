515666c7c529aff20c402c9e8564425a95990fac
banuelossoto-leticia <banuelossoto.leticia@gmail.com>
2020-10-22 13:40:53 -0700

    Added the content to the AddTransactionActivity as well as add functionality to the navigation buttons. Uploaded more xml files into the folder. Added the functionality to the back and submit buttons. Added the TransactionConfirmationActivity. Added the functionality of the navigation buttons on TransactionConfirmationActivity. Made TransactionConfirmationActivity to only show for a few seconds before going to HomeScreenActivity

---
+++ app/src/main/java/com/example/cecs448/TransactionConfirmationActivity.java
@@ -0,0 +1,54 @@
+package com.example.cecs448;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.ImageButton;
+
+public class TransactionConfirmationActivity extends AppCompatActivity {
+
+    private static int SPLASH_TIME_OUT = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_transaction_confirmation);
+
+        new Handler().postDelayed(new Runnable(){
+
+            @Override
+            public void run() {
+                //this method will be executed once the timer is over
+                //this is for the splash screen, start app with HomeScreenActivity
+                Intent i = new Intent(TransactionConfirmationActivity.this, HomeScreenActivity.class);
+                startActivity(i);
+
+                //closes this activity
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+
+        //this is the button for the pieChartActivity from AddTransactionActivity
+        ImageButton pieChartButton = (ImageButton) findViewById(R.id.pieChartButton5);
+        pieChartButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(TransactionConfirmationActivity.this, PieChartActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        //this the button for the transactionListActivity from AddTransactionActivity
+        ImageButton transactionListButton = (ImageButton) findViewById(R.id.transactionListButton5);
+        transactionListButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(TransactionConfirmationActivity.this, TransactionListActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
