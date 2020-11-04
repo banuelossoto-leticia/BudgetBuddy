0d76f12e219979de13e3cd4d7c3e791c6958fd74
banuelossoto-leticia <banuelossoto.leticia@gmail.com>
2020-11-03 23:32:55 -0800

    added more drawables. Created a transaction object class, as well as a TransactionAdapter class so that the ArrayList of object Transaction can be displayed in the recycler view for TransactionListActivity. Functionality for TransactionListActivity works, each transaction is clickable.

---
+++ app/src/main/java/com/example/cecs448/Transaction.java
@@ -0,0 +1,31 @@
+package com.example.cecs448;
+
+import androidx.annotation.NonNull;
+
+import java.io.Serializable;
+
+//implements serializable because it will get use in multiple activities
+public class Transaction implements Serializable {
+    //private classes
+    private String category;
+    private Double amount;
+    private String note;
+
+    public Transaction(String cate, Double amoun, String not){
+        this.category = cate;
+        this.amount = amoun;
+        this.note = not;
+    }
+
+    public String getCategory(){
+        return this.category;
+    }
+
+    public Double getAmount(){
+        return this.amount;
+    }
+
+    public String getNote(){
+        return this.note;
+    }
+}
--- app/src/main/java/com/example/cecs448/TransactionListActivity.java
+++ app/src/main/java/com/example/cecs448/TransactionListActivity.java
@@ -1,38 +1,80 @@
 package com.example.cecs448;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.TextView;
 
-public class TransactionListActivity extends AppCompatActivity implements View.OnClickListener {
-    ImageView transList;
+import org.w3c.dom.Text;
+
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+
+public class TransactionListActivity extends AppCompatActivity implements View.OnClickListener, TransactionsAdapter.OnTransactionListener {
     ImageButton homeBtn, pieBtn;
+    RecyclerView rvTransaction;
+    TextView actualTotalSpentTextView;
+    double totalSpent = 0;
+    ArrayList<Transaction> transactions;
+    TransactionsAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_transaction_list);
-        transList = findViewById(R.id.listOfTrans);
+
         homeBtn = findViewById(R.id.homeButton3);
         pieBtn = findViewById(R.id.pieChartButton3);
 
-        transList.setOnClickListener(this);
         homeBtn.setOnClickListener(this);
         pieBtn.setOnClickListener(this);
+
+        //for the two place decimal
+        DecimalFormat df = new DecimalFormat("#.##");
+
+        //creating dummy transactions
+        transactions = new ArrayList<Transaction>();
+
+        transactions.add(new Transaction("BILLS",45.34, "This is electricity bill"));
+        transactions.add(new Transaction("BILLS",400.00, "this is rent"));
+        transactions.add(new Transaction("FUN",23.23,"go-kart"));
+
+        //creating recycler view
+        //if it dosen't work try getApplicationContext()
+        adapter = new TransactionsAdapter(transactions, getApplicationContext(),this);
+
+        rvTransaction = (RecyclerView) findViewById(R.id.listRecycleView);
+        rvTransaction.setHasFixedSize(true);
+
+        LinearLayoutManager manager = new LinearLayoutManager(this);
+        rvTransaction.setLayoutManager(manager);
+        rvTransaction.setAdapter(adapter);
+
+        //cvTransaction = (CardView) findViewById(R.id.cvTransaction);
+
+        //sets the actualTotalSpentTextView to be total
+        //loops through the transaction list and adds up the total
+
+        for(int i = 0; i < transactions.size(); i++){
+            totalSpent = totalSpent + transactions.get(i).getAmount();
+        }
+
+        actualTotalSpentTextView = (TextView) findViewById(R.id.actualTotalSpentTextView);
+        actualTotalSpentTextView.setText("$" + String.valueOf(df.format(totalSpent)));
+
 
     }
 
     @Override
     public void onClick(View v) {
         switch(v.getId()) {
-            case R.id.listOfTrans:
-                startActivity(new Intent(getApplicationContext(), TransactionDetails.class));
-                finish();
-                break;
             case R.id.homeButton3:
                 startActivity(new Intent(getApplicationContext(), HomeScreenActivity.class));
                 finish();
@@ -44,4 +86,12 @@ public class TransactionListActivity extends AppCompatActivity implements View.O
         }
     }
 
+    @Override
+    public void onTransactionClick(int position) {
+        transactions.get(position);
+        Intent intent = new Intent(this, TransactionDetails.class);
+        //attaching
+        intent.putExtra("positionOfTransactionList",transactions.get(position));
+        startActivity(intent);
+    }
 }
